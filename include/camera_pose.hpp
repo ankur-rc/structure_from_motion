2c4a670323091e9934fa06ac1eccb018f917e0c8
Ankur Roy Chowdhury <ankurrc.1993@gmail.com>
2019-04-30 15:04:50 -0500

    Constructing triangulated points before bundle adjustment.

---
+++ include/camera_pose.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include <opencv2/core.hpp>
+
+struct CameraPose
+{
+
+    cv::Mat T;                           // transform [R|t]
+    cv::Mat P;                           // projection K*T
+    cv::Mat image;                       // downsampled image
+    std::vector<cv::KeyPoint> keypoints; // list of keypoints
+    cv::Mat descriptors;                 // matrix of keypoint descriptors
+
+    std::map<size_t, std::map<size_t, size_t>> keypoint_match; // maps current camera's keypoint id to other cameras' keypoint id
+    std::map<size_t, size_t> keypoint_landmark;                // maps a keypoint id to a landmark id
+
+    bool is_keypoint_exists(size_t keypoint_id, size_t camera_index)
+    {
+        return keypoint_match[keypoint_id].count(camera_index) > 0;
+    }
+
+    const size_t &get_keypoint_match(size_t keypoint_id, size_t camera_index)
+    {
+        return keypoint_match[keypoint_id][camera_index];
+    }
+
+    bool is_keypoint_landmark(size_t keypoint_id)
+    {
+        return keypoint_landmark.count(keypoint_id) > 0;
+    }
+
+    size_t &get_keypoint_landmark(size_t keypoint_id)
+    {
+        return keypoint_landmark[keypoint_id];
+    }
+};
+
+struct Landmark
+{
+    cv::Point3f pt3d;
+    cv::Vec3b color;
+    size_t id;
+    uint visible = 0;
+};
